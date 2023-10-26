2fbfb0f679f6d04599c01a6b5609656698d8bb31
navyashukla10 <96282433+navyashukla10@users.noreply.github.com>
2023-10-26 19:27:55 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -1,6 +1,11 @@
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
+
 struct BigInteger* Initialize(int digit);
 struct BigInteger* add(struct BigInteger* n1, struct BigInteger* n2);
 struct BigInteger* sub(struct BigInteger* n1, struct BigInteger* n2);
 struct BigInteger* mul(struct BigInteger* n1, struct BigInteger* n2);
 struct BigInteger* div1(struct BigInteger* dividend, struct BigInteger* divisor);
 void disp(struct BigInteger* h);
+
+#endif
